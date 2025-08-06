cc063a58aa3c0d4ac23830484e36fae0018201ce
Abduraxmon <abduraimovabdurahmon@gmail.com>
2025-08-06 13:17:39 +0500

    Sidebarga elementlar qo'shildi.

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,14 +1,14 @@
 export default {
-  logo: <strong>Linux Lab</strong>,
-  project: {
-    link: "https://github.com/abduraimovabdurahmon/linuxlab.uz",
-  },
-  useNextSeoProps() {
-    return {
-      titleTemplate: "LinuxLab.uz - %s",
-    };
-  },
-  footer: {
-    text: "Copyright Abdurakhmon 2025",
-  }
+    logo: <span style={{ display: "inline-flex", alignItems: "center" }}>Linux <img src="/terminal.webp" alt="Icon" style={{ marginLeft: "5px", width:20 }} /></span>,
+    project: {
+        link: "https://github.com/abduraimovabdurahmon/linuxlab.uz",
+    },
+    useNextSeoProps() {
+        return {
+            titleTemplate: "LinuxLab.uz - %s",
+        };
+    },
+    footer: {
+        text: "Copyright Abdurakhmon 2025",
+    }
 };
