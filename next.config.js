1cd772a45cdbdb68159746a70e09349f8ec566f4
Abduraxmon <abduraimovabdurahmon@gmail.com>
2025-08-06 15:21:04 +0500

    Vercelni sinab ko'rish uchun test commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
+};
 
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
 });
 
 module.exports = withNextra(nextConfig);
