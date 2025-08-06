2edd3ab4dbab60eb2b3fb3f8de8dcc2d8e88101b
Abduraxmon <abduraimovabdurahmon@gmail.com>
2025-08-06 12:40:57 +0500

    Create project

---
+++ theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+  logo: <strong>Linux Lab</strong>,
+  project: {
+    link: "https://github.com/abduraimovabdurahmon/linuxlab.uz",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "LinuxLab.uz - %s",
+    };
+  },
+  footer: {
+    text: "Copyright Abdurakhmon 2025",
+  }
+};
