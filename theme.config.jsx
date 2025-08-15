5f8ab06a8215692aa13280a784b958f838aad97b
Abduraxmon <abduraimovabdurahmon@gmail.com>
2025-08-15 16:15:27 +0500

    Java ga o'zgartirishlar kiritdim

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,8 @@
 export default {
     logo: <span style={{fontWeight: "bold"}}>Linux Lab</span>,
+    sidebar: {
+        defaultMenuCollapseLevel: 1,
+    },
     project: {
         link: "https://github.com/abduraimovabdurahmon/linuxlab.uz",
     },
